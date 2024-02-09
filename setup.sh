7209cc6baa518b3b56a7a25598e86f8e807c0fbf
root <joshlevy89@gmail.com>
2024-02-09 22:30:59 +0000

    Added intervention expts

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ pip uninstall torch torchvision torchaudio -y
 pip3 install transformers>=4.32.0 optimum>=1.12.0
 pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 --index-url https://download.pytorch.org/whl/cu121
 pip install auto-gptq
+pip install tokenizers==0.13.3
